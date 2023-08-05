ede0974100a33b5467a443222f0a8dd4671c269b
Whyukim <110218594+Whyukim@users.noreply.github.com>
2023-08-05 12:12:12 +0900

    express

---
+++ express/app.js
@@ -0,0 +1,23 @@
+import express from "express";
+const app = express();
+app.use('/api', (req, res, next) => {
+    console.log('all');
+})
+app.all('/api', (req, res, next) => {
+    console.log('all');
+})
+
+app.get(
+  "/",
+  (req, res, next) => {
+    console.log('first');
+  },
+  (req, res, next) => {
+    console.log("first2");
+  }
+);
+
+app.get('/', (res, req, next) => {
+    console.log('second');
+})
+app.listen(8080);
