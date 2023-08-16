2cddca22c9c98ad669e646c27c7db26c0905fdca
Whyukim <110218594+Whyukim@users.noreply.github.com>
2023-08-16 12:12:12 +0900

    update

--- 12-back/router/auth.js
+++ 12-back/router/auth.js
@@ -4,21 +4,34 @@ import { body } from "express-validator";
 
 import * as authController from "../controller/auth.js";
 import { validate } from "../middleware/validator.js";
+import { isAuth } from "../middleware/auth.js";
 
 const router = express.Router();
 
-const validateTweet = [
-  body("text").trim().isLength({ min: 2 }).withMessage("2글자 이상"),
+const validateLogin = [
+  body("username").trim().notEmpty().withMessage("5글자 이상"),
+  body("password").trim().isLength().withMessage("5글자 이상"),
   validate,
 ];
 
-// GET /me
-router.get("/me", authController.getMe);
+const validateSignup = [
+  ...validateLogin,
+  body("name").notEmpty().withMessage("5글자 이상"),
+  body("email").isEmail().normalizeEmail().withMessage("invalid email"),
+  body("url")
+    .isURL()
+    .withMessage("invalid url")
+    .optional({ nullable: true, checkFalsy: true }),
+  validate,
+];
 
 // POST /login
-router.post("/login", authController.login);
+router.post("/login", validateLogin, authController.login);
 
 // POST /signin
-router.post("/signup", authController.create);
+router.post("/signup", validateSignup, authController.signup);
+
+// GET /me
+router.get("/me", isAuth, authController.me);
 
 export default router;
